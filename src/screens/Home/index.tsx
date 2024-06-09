5fe19d120ae37fd72149a84cb32a9b1e6fbcce3a
MiguelEduardo-Mazocho <dardoprogrammer@gmail.com>
2024-06-09 10:11:37 -0300

    Aqui fiz a tipagem do Navigation

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,10 @@
 import { Container } from './styles'
 
 import { HomeHeader } from '../../components/HomeHeader'
+import { useNavigation } from '@react-navigation/native'
 
 export function Home() {
+  
   return (
     <Container>
       <HomeHeader />
